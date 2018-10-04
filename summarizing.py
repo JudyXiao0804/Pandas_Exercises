f22b79a4724a228538c134afea6b4676b39ae1d1
XiaoJing <40845886+JudyXiao0804@users.noreply.github.com>
2018-10-05 00:33:34 +0900

    Create summarizing.py

---
+++ summarizing.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+
+import numpy as np
+from pandas import Series, DataFrame
+
+
+df = DataFrame([[1.4, np.nan], [7.1, -4.5], [np.nan, np.nan], [0.75, -1.3]],
+              index = ['a', 'b', 'c', 'd'],
+              columns = ['one', 'two'])
+print df
+print df.sum()
+print df.sum(axis = 1)  
+print
+
+
+print df.mean(axis = 1, skipna = False)
+print df.mean(axis = 1)
+print
+
+
+print df.idxmax()
+print df.cumsum()
+print df.describe()
+obj = Series(['a', 'a', 'b', 'c'] * 4)
+print obj.describe()
